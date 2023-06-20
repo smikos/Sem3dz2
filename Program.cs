a7e72120226122933ddb4b837dfbda07f9f4f5ae
Konstantin Smirnov <sasharugby@yandex.ru>
2023-06-20 21:24:16 +0300

    kordinati xYz

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿int ReadInt(string message)
+{
+    System.Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+double length( int x, int y, int z)
+{
+    double temp = (x*x)+(y*y)+(z*z);
+    return Math.Sqrt(temp);
+}
+
+
+
+int x1 = ReadInt("Введите координау X первой точки ");
+int y1 = ReadInt("Введите координау Y первой точки ");
+int z1 = ReadInt("Введите координау Z первой точки ");
+int x2 = ReadInt("Введите координау X второй точки ");
+int y2 = ReadInt("Введите координау Y второй точки ");
+int z2 = ReadInt("Введите координау Z второй точки ");
+
+
+
+double res = length (x1-x2,y1-y2,z1-z2);
+double res2 = Math.Sqrt( ((x1-x2)*(x1-x2))+((y1-y2)*(y1-y2))+((z1-z2)*(z1-z2)));// Проверка 
+Console.WriteLine ( res);
+Console.WriteLine ( res2);
